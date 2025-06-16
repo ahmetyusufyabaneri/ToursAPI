e6c6cac3cd6773fa23b4ebe9be99173d1b062925
ahmetyusufyabaneri <ahmetyusuf1yabaneri@gmail.com>
2025-06-16 03:12:27 +0300

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 const signToken = (userId) => {
   return jwt.sign({ id: userId, role: "admin" }, process.env.JWT_SECRET, {
